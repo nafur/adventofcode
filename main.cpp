c96a1feb730cf31c3149ddb42cbf7b07b1495a8c
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2017-12-01 10:15:28 +0100

    Added solutions for first two puzzles.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,8 @@
 #include "utils/framework.h"
 
+#include "1.h"
+#include "2.h"
+
 int main(int argc, char** argv) {
 	runSolver<3>();
 }
---
+++ utils/strings.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <string_view>
+
+int sum_if_same(const std::string_view& sv, std::size_t offset) {
+	int sum = 0;
+	for (std::size_t cur = 0; cur < sv.size(); ++cur) {
+		std::size_t other = (cur + offset) % sv.size();
+		if (sv[cur] == sv[other]) sum += sv[cur] - '0';
+	}
+	return sum;
+}
